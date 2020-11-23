0b953846f7955d9057f9309125390238aa5b5652
Lukasz Charubin <lukasz.charubin@gmail.com>
2020-11-23 12:56:58 +0100

    Init commit.

---
+++ src/components/HiitCreate.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script lang="ts">
+
+import { Component, Prop, Vue } from 'vue-property-decorator';
+@Component
+export default class HiitCreate extends Vue {
+    @Prop() private title!: string;
+    setup () {
+        return {}
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
