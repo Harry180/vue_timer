5853156a569abd785ad57b4eb84ee55050801623
Lukasz Charubin <lukasz.charubin@gmail.com>
2020-11-23 13:01:38 +0100

    Add router.

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HiitCreate title="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HiitCreate from '@/components/HiitCreate.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HiitCreate
+  }
+}
+</script>
